5b83f20a3bf5245d8ff3868b0aca07628b3096c6
Troy Chmieleski <tchmieleski@incubate.co>
2014-04-06 12:17:59 -0500

    Created Xcode Project

---
+++ Troy ChmieleskiTests/Troy_ChmieleskiTests.m
@@ -0,0 +1,34 @@
+//
+//  Troy_ChmieleskiTests.m
+//  Troy ChmieleskiTests
+//
+//  Created by Troy Chmieleski on 4/6/14.
+//  Copyright (c) 2014 Troy Chmieleski. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Troy_ChmieleskiTests : XCTestCase
+
+@end
+
+@implementation Troy_ChmieleskiTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
