ca0f1e0cd31cdfe73705c3adf58101040383080e
Troy Chmieleski <tchmieleski@incubate.co>
2014-04-08 21:33:51 -0500

    Added education reflection model

---
+++ Troy Chmieleski/EducationReflection.h
@@ -0,0 +1,18 @@
+//
+//  EducationReflection.h
+//  Troy Chmieleski
+//
+//  Created by Troy Chmieleski on 4/7/14.
+//  Copyright (c) 2014 Troy Chmieleski. All rights reserved.
+//
+
+#import "Reflection.h"
+
+@interface EducationReflection : Reflection
+
+@property (nonatomic, strong) NSString *school;
+@property (nonatomic, strong) NSString *degree;
+@property (nonatomic, strong) NSString *expectedGraduationDate;
+@property (nonatomic, strong) NSString *gpa;
+
+@end
---
+++ Troy Chmieleski/EducationReflection.m
@@ -0,0 +1,13 @@
+//
+//  EducationReflection.m
+//  Troy Chmieleski
+//
+//  Created by Troy Chmieleski on 4/7/14.
+//  Copyright (c) 2014 Troy Chmieleski. All rights reserved.
+//
+
+#import "EducationReflection.h"
+
+@implementation EducationReflection
+
+@end
