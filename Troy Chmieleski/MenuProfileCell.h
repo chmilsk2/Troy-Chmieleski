8210eadb942797363cd76a4bc430ad767881115c
Troy Chmieleski <tchmieleski@incubate.co>
2014-04-08 21:36:29 -0500

    added menu profile cell

---
+++ Troy Chmieleski/MenuProfileCell.h
@@ -0,0 +1,13 @@
+//
+//  MenuProfileCell.h
+//  Troy Chmieleski
+//
+//  Created by Troy Chmieleski on 4/8/14.
+//  Copyright (c) 2014 Troy Chmieleski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuProfileCell : UITableViewCell
+
+@end
---
+++ Troy Chmieleski/MenuProfileCell.m
@@ -0,0 +1,30 @@
+//
+//  MenuProfileCell.m
+//  Troy Chmieleski
+//
+//  Created by Troy Chmieleski on 4/8/14.
+//  Copyright (c) 2014 Troy Chmieleski. All rights reserved.
+//
+
+#import "MenuProfileCell.h"
+
+@implementation MenuProfileCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+	
+    if (self) {
+        // Initialization code
+    }
+	
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
